937b5917062eafe86f1567413554eeceb81ecaee
shahzodbeeck <shahzodomonboyev1.com>
2024-08-06 16:31:15 +0500

    asdasdd

--- backend/transfer/api.py
+++ backend/transfer/api.py
@@ -1,11 +1,12 @@
 # from backend.transfer.group.request import transfer_course_types
 # from backend.transfer.payments.request import transfer_payment_types
 # from backend.transfer.subjects.request import transfer_subjects, transfer_subject_levels
-from backend.transfer.branch_location.reuqest import transfer_location, transfer_branch
+from backend.transfer.job.request import transfer_job
 from language.request import transfer_language
 from login.request import login
 
-# login = login()
+login = login()
 # transfer_location()
 # transfer_branch(login)
-transfer_language()
+# transfer_language()
+# transfer_job()
